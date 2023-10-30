10bdbcc23013887a3077bebb3cd2c3b708309d16
Paketgit <maxim.ipati2006@gmail.com>
2023-10-30 22:29:43 +0300

    Создание коннекта с бд

--- test_syst.py
+++ test_syst.py
@@ -2,15 +2,6 @@ import json
 import sqlite3
 
 
-def stalc_handler(lots=''):
-    inputlots_find = lots
-    inputlots = lots.lower().title()
-    with open('./ru/listing.json', 'r', encoding='UTF-8') as all_list:
-        records = json.load(all_list)
-    for i in records:
-
-        return (i['data'][-9:-5], i['name']['lines']['ru'])
-
 def update_result():
     con = sqlite3.connect('sc_db')
     cur = con.cursor()
@@ -18,7 +9,12 @@ def update_result():
         records = json.load(all_list)
 
     for i in records:
-        print(f"""INSERT INTO items(name, game_code) VALUES('{i['name']['lines']['ru']}', '{i['data'][-9:-5]}')""")
-        cur.execute(f"""INSERT INTO items(name, game_code) VALUES('{i['name']['lines']['ru']}', '{i['data'][-9:-5]}')""")
+        try:
+            print(f"""INSERT INTO items(name, game_code) VALUES('{i['name']['lines']['ru']}', '{i['data'][-9:-5]}')""")
+            cur.execute(f"""INSERT INTO items(name, game_code) VALUES('{i['name']['lines']['ru']}', '{i['data'][-9:-5]}')""")
+            print('OK')
+        except sqlite3.IntegrityError:
+            print('NO')
+            pass
 
 update_result()
