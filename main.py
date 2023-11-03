96a8c09a18abf8548d1a0254294e99a011a7a009
gimonchik <fedorgorlenko@gmail.com>
2023-11-03 13:30:52 +0300

    осталось реализовать бд и разобраться с масштабом matplotlib

--- main.py
+++ main.py
@@ -4,19 +4,16 @@ import sys
 import datetime
 
 import matplotlib.pyplot as plt
-import numpy as np
+from matplotlib import rcParams
 from PIL import Image
 from PyQt5 import uic
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QMessageBox
 
 import Auth
 
+rcParams.update({'font.size': 10})
 conn = http.client.HTTPSConnection("eapi.stalcraft.net")
-#берём отсюда цены
-#data = stalc_handler(text)
-#for i in data:
-#    print(i['buyoutPrice'])
 
 
 class Sc_helper(QMainWindow):
@@ -30,18 +27,27 @@ class Sc_helper(QMainWindow):
     def __get_grpah(self):
         lots = self.Input_item.toPlainText()
         print(lots)
-        x, y = stalc_handler(lots)
-        self.aucList.setRowCount(1)
-        self.aucList.setColumnCount(1000)
+        x, y = self.stalc_handler(lots)
+        if x == "Error":
+            return 0
+        self.aucList.setColumnCount(2)
         print('1')
         try:
-            for i in range(len(x)):
-                self.aucList.setItem(i, 1, x[i])
+            self.aucList.setRowCount(len(y))
+            for i in range(len(y)):
+                self.aucList.setItem(i - 1, 2, QTableWidgetItem(str(y[i])))
+                self.aucList.setItem(i, 1, QTableWidgetItem(x[i]))
+                print(x[i], y[i])
         except Exception:
-            print('DOLBAEB')
+            print('ОШИБКА')
+
+        print('-------------------------')
         print(x, y)
-        print(x)
         plt.plot(x, y)
+        rcParams.update({'font.size': 5})
+        plt.xlabel('Дата', fontsize=10)
+        plt.ylabel(r'Цены', fontsize=10)
+        plt.semilogy()
         plt.savefig('Graph')
         img = Image.open('Graph.png')
         # изменяем размер
@@ -52,48 +58,46 @@ class Sc_helper(QMainWindow):
         self.Graph_time.setPixmap(self.pixmap)
         self.show()
 
+    def __sortkey(self, dataset):
+        return dataset['buyoutPrice']
 
-def sortkey(dataset):
-    return dataset['buyoutPrice']
+    def err(self):
+        QMessageBox.question(self, 'Error',
+                             'Проверьте интернет соединение или написание лота', QMessageBox.Ok)
 
-
-def stalc_handler(lots):
-    print('Я в stalc_handler')
-    inputlots_find = lots
-    inputlots = lots.lower().title()
-    money, time = 0, ''
-    with open('./ru/listing.json', 'r', encoding='UTF-8') as all_list:
-        # комнуздим из файла как записан тот или иной итем
-        records = json.load(all_list)
-    for i in records:  # Это чудо инжинерной мысли не трогать
-        try:
+    def stalc_handler(self, lots):
+        print('Я в stalc_handler')
+        inputlots_find = ''
+        inputlots = lots.lower().title()
+        with open('./ru/listing.json', 'r', encoding='UTF-8') as all_list:
+            # комнуздим из файла как записан тот или иной итем
+            records = json.load(all_list)
+        for i in records:  # Это чудо инжинерной мысли не трогать
             if inputlots == i['name']['lines']['ru']:
                 inputlots_find = i['data'][-9:-5]
                 print(i['data'][-9:-5])
-            if inputlots == 'all':
-                print(i['name']['lines']['ru'], i['data'][-9:-5])
-        except KeyError:
-            print('eblan')
-            pass
-    print('Я прошёл файл')
-    conn.request("GET", f"/RU/auction/{inputlots_find}/lots?limit=200&additional=true", headers=Auth.headers)
-    print('conn ok')
-    rowdata = conn.getresponse().read().decode('utf-8')
-    print('rowdata ok')
-    data = json.loads(rowdata)
-    print('data ok')
-    data = data['lots']
-    print("Я прошёл конн и получение данныых")
-    data = sorted(data, key=sortkey)
-    print(data)
-    money_lots = []
-    date_lots = []
-    for i in data:
-        date_lots.append(i['endTime'])
-        money_lots.append(i['buyoutPrice'])
-    print(money_lots, date_lots, sep='\n')
-    print('Я вышел')
-    return date_lots, money_lots
+        if not inputlots_find:
+            self.err()
+            return 'Error', 0
+        print('Я прошёл файл')
+        conn.request("GET", f"/RU/auction/{inputlots_find}/lots?limit=200&additional=true", headers=Auth.headers)
+        print('conn ok')
+        rowdata = conn.getresponse().read().decode('utf-8')
+        print('rowdata ok')
+        data = json.loads(rowdata)
+        print('data ok')
+        data = data['lots']
+        print("Я прошёл конн и получение данныых")
+        data = sorted(data, key=self.__sortkey)
+        print(data)
+        money_lots = []
+        date_lots = []
+        for i in data:
+            date_lots.append(i['endTime'])
+            money_lots.append(i['buyoutPrice'])
+        print(money_lots, date_lots, sep='\n')
+        print('Я вышел')
+        return date_lots, money_lots
 
 
 if __name__ == '__main__':
