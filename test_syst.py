4dfa787d552f64ca50f03e6a00c7b814e8dd092f
Paketgit <maxim.ipati2006@gmail.com>
2023-11-10 02:09:50 +0300

    меняем реад ме

--- test_syst.py
+++ test_syst.py
@@ -1,33 +1,20 @@
-import json
-import sqlite3
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+
+import matplotlib as mpl
 
 
-def update_result():
-    con = sqlite3.connect('sc_db')
-    cur = con.cursor()
-    with open('./ru/listing.json', 'r', encoding='UTF-8') as all_list:
-        records = json.load(all_list)
 
-    for i in records:
-        #print(f"""INSERT INTO items(name, game_code) VALUES('{i['name']['lines']['ru']}', '{i['data']}')""")
-        with open('./ru/'+i['data'], 'r', encoding='UTF-8') as f:
-            info = json.load(f)
-        try:
-            print(info['infoBlocks'][-1]['text']['lines']['ru'])
-            print(
-                f"""UPDATE items SET game_info = "{info['infoBlocks'][-1]['text']['lines']['ru']}" 
-                WHERE game_code = '{i['data'][-9:-5]}'""")
-            cur.execute(
-                f"""UPDATE items SET game_info = "{info['infoBlocks'][-1]['text']['lines']['ru']}" 
-                WHERE game_code = '{i['data'][-9:-5]}'""")
+x = np.arange(0, 10, 0.1)
+y = np.sin(x)
 
-            print('Ok')
-        except Exception:
-            print('Error')
-            pass
-    con.commit()
+plt.plot(x, y)
 
-    con.close()
-
-
-update_result()
+plt.savefig('Graph')
+img = Image.open('Graph.png')
+# изменяем размер
+new_image = img.resize((400, 300))
+new_image.show()
+# сохранение картинки
+new_image.save('./Graph.png')
